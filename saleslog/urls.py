417cd9888efdf2f63bda33ff5d54c8d42d684366
Justin Thomas <justin.don.thomas@gmail.com>
2022-04-21 05:28:37 -0600

    Add view and add page skeletons.

--- saleslog/urls.py
+++ saleslog/urls.py
@@ -2,6 +2,9 @@ from django.urls import include
 from django.urls import path
 from . import views
 
+app_name = 'saleslog'
 urlpatterns = [
-  path('', views.index, name='index')
+  path('', views.index, name='index'),
+  path('view_listings/', views.view_listings, name='view_listings'),
+  path('add_listing/', views.add_listing, name='add_listing')
 ]
--- saleslog/views.py
+++ saleslog/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def index(request):
   context = {}
   return render(request, 'saleslog/index.html', context=context)
+
+def view_listings(request):
+  context = {}
+  return render(request, 'saleslog/view_listings.html', context=context)
+
+def add_listing(request):
+  context={}
+  return render(request, 'saleslog/add_listing.html', context=context)
