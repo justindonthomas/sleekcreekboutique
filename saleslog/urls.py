d81000a695f8511d21940261a4ff47269dc15193
Justin Thomas <justin.don.thomas@gmail.com>
2022-04-21 06:14:35 -0600

    Fix url construction.

--- saleslog/urls.py
+++ saleslog/urls.py
@@ -6,5 +6,5 @@ app_name = 'saleslog'
 urlpatterns = [
   path('', views.index, name='index'),
   path('view_listings/', views.view_listings, name='view_listings'),
-  path('add_listing/', views.add_listing, name='add_listing')
+  path('add_listing/', views.add_listing, name='add_listing'),
 ]
