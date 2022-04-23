7d0e394ddd2f7ae2e3a4698a3c78f1dc236afe71
Justin Thomas <justin.don.thomas@gmail.com>
2022-04-23 05:05:12 -0600

    Tabs to spaces

--- saleslog/models.py
+++ saleslog/models.py
@@ -3,77 +3,80 @@ from django.core.validators import MinValueValidator
 from django.db import models
 
 class AbstractThing(models.Model):
-  name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
 
-  class Meta:
-    abstract = True
+    class Meta:
+        abstract = True
 
 class Character(AbstractThing):
-  """
-  Name of a character.
-  """
-  pass
+    """
+    Name of a character.
+    """
+    pass
 
 class Guild(AbstractThing):
-  """
-  Name of a guild.
-  """
-  pass
+    """
+    Name of a guild.
+    """
+    pass
 
 class Item(AbstractThing):
-  """
-  Name of an item.
-  """
-  pass
+    """
+    Name of an item.
+    """
+    pass
 
 class Location(AbstractThing):
-  """
-  Name of a location.
-  """
-  pass
+    """
+    Name of a location.
+    """
+    pass
 
 class Listing(models.Model):
-  """
-  Guild store listing entry.
+    """
+    Guild store listing entry.
 
-  item        Item for sale.
-  quantity    Number of items for sale.
-  total_price Total sell price of listing.
-  end_date    Estimated end time of listing.
-  guild       Name of the selling guildstore.
-  location    Name of the location of the guild trader.
-  character   Name of the character selling the item.
-  posted_by   Auth user who created the listing.
-  """
-  item = models.ForeignKey(Item, on_delete=models.CASCADE)
-  quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
-  total_price = models.PositiveBigIntegerField(validators=[MinValueValidator(1)])
-  end_date = models.DateTimeField()
-  guild = models.ForeignKey(Guild, on_delete=models.DO_NOTHING, blank=True, null=True)
-  location = models.ForeignKey(Location, on_delete=models.DO_NOTHING, blank=True, null=True)
-  character = models.ForeignKey(Character, on_delete=models.DO_NOTHING, blank=True, null=True)
-  posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    item        Item for sale.
+    quantity    Number of items for sale.
+    total_price Total sell price of listing.
+    post_date   Date listing created in database (not necessarily real posting
+                date).
+    end_date    Estimated end time of listing.
+    guild       Name of the selling guildstore.
+    location    Name of the location of the guild trader.
+    character   Name of the character selling the item.
+    posted_by   Auth user who created the listing.
+    """
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    total_price = models.PositiveBigIntegerField(validators=[MinValueValidator(1)])
+    post_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    guild = models.ForeignKey(Guild, on_delete=models.DO_NOTHING, blank=True, null=True)
+    location = models.ForeignKey(Location, on_delete=models.DO_NOTHING, blank=True, null=True)
+    character = models.ForeignKey(Character, on_delete=models.DO_NOTHING, blank=True, null=True)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Sale(models.Model):
-  """
-  Sale of a listing.
+    """
+    Sale of a listing.
 
-  listing     Listing that was sold.
-  net_price   Amount received by player for sale (Listing total_price minus
-              fees).
-  """
-  listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
-  net_price = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    listing     Listing that was sold.
+    net_price   Amount received by player for sale (Listing total_price minus
+                            fees).
+    """
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    net_price = models.PositiveIntegerField(validators=[MinValueValidator(1)])
 
 class UserCharacter(models.Model):
-  """
-  Map of users to characters.
+    """
+    Map of users to characters.
 
-  user      auth User object.
-  character Associated character.
-  """
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  character = models.ForeignKey(Character, on_delete=models.CASCADE)
+    user      auth User object.
+    character Associated character.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
 
 
 
--- saleslog/views.py
+++ saleslog/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  context = {}
-  return render(request, 'saleslog/index.html', context=context)
+    context = {}
+    return render(request, 'saleslog/index.html', context=context)
 
 def view_listings(request):
-  context = {}
-  return render(request, 'saleslog/view_listings.html', context=context)
+    context = {}
+    return render(request, 'saleslog/view_listings.html', context=context)
 
 def add_listing(request):
-  context={}
-  return render(request, 'saleslog/add_listing.html', context=context)
+    context={}
+    return render(request, 'saleslog/add_listing.html', context=context)
