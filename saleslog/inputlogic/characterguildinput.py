fc750a8db09cc27a0711ce5dc027f0eaf87c6679
Justin Thomas <justin.don.thomas@gmail.com>
2022-04-30 04:48:24 -0600

    Fix reset buttons

--- saleslog/inputlogic/characterguildinput.py
+++ saleslog/inputlogic/characterguildinput.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import MultipleObjectsReturned
 from django.db  import IntegrityError
+from saleslog.forms import GuildInput
 from saleslog.inputlogic.formprocessor import FormProcessor
 from saleslog.models import Character, CharacterGuild, Guild, Location
 
@@ -35,7 +36,7 @@ class CharacterGuildInput(FormProcessor):
                 if item:
                     if item['DELETE']:
                         try:
-                            guild = Guild.objects.get(name__iexact=item['guild'])
+                            guild = Guild.objects.get(name__iexact=item[GuildInput.GUILD_NAME])
                         except (Guild.DoesNotExist, MultipleObjectsReturned) as e:
                             print(e)
                             return False
@@ -47,12 +48,15 @@ class CharacterGuildInput(FormProcessor):
                             print(e)
                             return False
                     else:
-                        location,_ = Location.objects.get_or_create(name=item['store_location'])
-                        guild,_ = Guild.objects.get_or_create(name=item['guild'])
-                        guild.store_location = location
+                        
+                        guild,_ = Guild.objects.get_or_create(name=item[GuildInput.GUILD_NAME])
+                        if item[GuildInput.LOCATION_NAME] and not item[GuildInput.LOCATION_NAME].isspace():
+                            location,_ = Location.objects.get_or_create(name=item[GuildInput.LOCATION_NAME])
+                            guild.store_location = location
                         guild.save()
                         charGuild,_ = CharacterGuild.objects.get_or_create(guild=guild,
                                                                 character=character)
+                        charGuild.is_primary = item[GuildInput.IS_PRIMARY]
                         try:
                             charGuild.full_clean()
                             charGuild.save()
